d220066265021b9493bf3b478890477b77ab32dd
saraiva142 <saraivajandrade@gmail.com>
2024-11-03 01:01:05 +0000

    Pessoa Fisica e Juridica OK

--- main.py
+++ main.py
@@ -36,8 +36,39 @@ with tab1:
             cliente.inserir_cliente()
             #ClienteController.Incluir(cliente)
             st.success('Cadastrado com sucesso!', icon="✅")
+            
+            # Armazenando a instância do cliente no session_state
+            st.session_state.cliente = cliente
     #else:
     #    st.error("É preciso preencher todos os campos")
+    # Formulário para inserir dados adicionais da Pessoa Física ou Jurídica
+    with st.form(key="include_pessoa"):
+        st.write("Preencher Tabela Pessoa")
+        # Reutilizar o cod_cliente para referenciar o cliente cadastrado
+        input_cod_cliente_pessoa = st.number_input("Código do Cliente (deve ser o mesmo cadastrado)", value=input_cod_cliente, format="%d", step=1)
+                
+        if input_tipo_cliente == "Pessoa Fisica":
+            input_nome_cli = st.text_input("Nome Completo")
+            input_cpf = st.text_input("CPF")
+        elif input_tipo_cliente == "Pessoa Juridica":
+            input_razao_social = st.text_input("Razão Social")
+            input_insc_estadual = st.text_input("Inscrição Estadual")
+            input_cnpj = st.text_input("CNPJ")
+                
+        input_pessoa_button_submit = st.form_submit_button(label="Enviar")
+
+        if input_pessoa_button_submit:
+            # Acessando a instância do cliente do session_state
+            cliente = st.session_state.get("cliente")
+            if cliente:  # Verifica se o cliente foi cadastrado anteriormente
+                if input_tipo_cliente == "Pessoa Fisica":
+                    cliente.inserir_pessoa_fisica(nome=input_nome_cli, cpf=input_cpf)
+                    st.success("Pessoa Física cadastrada com sucesso!", icon="✅")
+                elif input_tipo_cliente == "Pessoa Juridica":
+                    cliente.inserir_pessoa_juridica(razao_social=input_razao_social, insc_estadual=input_insc_estadual, cnpj=input_cnpj)
+                    st.success("Pessoa Jurídica cadastrada com sucesso!", icon="✅")
+
+        
 
 with tab2:
     st.title("CRUD ESTADO")
